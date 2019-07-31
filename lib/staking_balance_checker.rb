b3f8e4c4b0b425b13780a98e368c53667aadcf6f
flada-auxv <aseknock@gmail.com>
2019-07-31 22:43:46 +0900

    transform

--- lib/staking_balance_checker.rb
+++ lib/staking_balance_checker.rb
@@ -3,13 +3,55 @@ require 'net/http'
 
 class StakingBalanceChecker
   ENDPOINT = 'https://stargate.cosmos.network/staking/validators'
+  STATUSES = ['bonded', 'unbonding', 'unbonded']
 
   def initialize
+  end
+
+  def run
+    data = extract_all
+    data = transform(data)
+    load_to_s3(data)
+  end
+
+  def extract_all
+    STATUSES.map {|status| extract(status) }
+  end
+
+  def extract(status)
+    res = Net::HTTP.get_response(url_for(status))
+    raise unless res.code == '200'
+
+    JSON.parse(res.body)
+  end
+
+  def transform(data)
+    data.sort_by {|d| -d['tokens'] }.map.with_index(1) {|d, i|
+      {
+        moniker: d['description']['moniker'],
+        delegated_balance: d['tokens'],
+        rank: i,
+        status: status_to_s(d['status'])
+      }
+    }
+  end
+
+  def load_to_s3(data)
+
   end
 
   def url_for(status)
     url = URI.parse(ENDPOINT)
     url.query = "status=#{status}"
     url
+  end
+
+  def status_to_s(status)
+    case status
+    when 0 then 'unbonded'
+    when 1 then 'unbonding'
+    when 2 then 'bonded'
+    else raise ArgumentError('unknown status')
+    end
   end
 end
--- test/staking_balance_checker_test.rb
+++ test/staking_balance_checker_test.rb
@@ -10,4 +10,41 @@ class Test < Minitest::Test
   def test_url_for_when_status_is_bonded
     assert_equal URI.parse('https://stargate.cosmos.network/staking/validators?status=bonded'), @checker.url_for('bonded')
   end
+
+  def test_transform
+    data = [{
+      'description' => {'moniker' => 'kansa'},
+      'tokens' => 20,
+      'status' => 1
+    }, {
+      'description' => {'moniker' => 'naska'},
+      'tokens' => 5,
+      'status' => 0
+    }, {
+      'description' => {'moniker' => 'sanka'},
+      'tokens' => 100,
+      'status' => 2
+    }]
+
+    expected = [
+      {
+        moniker: 'sanka',
+        delegated_balance: 100,
+        rank: 1,
+        status: 'bonded'
+      },{
+        moniker: 'kansa',
+        delegated_balance: 20,
+        rank: 2,
+        status: 'unbonding'
+      },{
+        moniker: 'naska',
+        delegated_balance: 5,
+        rank: 3,
+        status: 'unbonded'
+      }
+    ]
+
+    assert_equal expected, @checker.transform(data)
+  end
 end
